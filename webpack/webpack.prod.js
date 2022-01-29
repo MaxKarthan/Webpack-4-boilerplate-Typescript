20aca88708537f82b2f9634691535cca080eeaf3
MaxKarthan <contact@max-karthan.de>
2022-01-29 15:50:23 +0100

    feat(build):add asset support

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 const { prod_Path, src_Path } = require("./path");
 const { selectedPreprocessor } = require("./loader");
@@ -41,7 +43,12 @@ module.exports = {
             loader: selectedPreprocessor.loaderName
           }
         ]
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+
     ]
   },
   plugins: [
@@ -54,6 +61,9 @@ module.exports = {
       hash: true,
       template: "./" + src_Path + "/index.html",
       filename: "index.html"
-    })
+    }),
+    new CopyWebpackPlugin({'patterns': [
+        {from:'./src/assets', to:'assets'}
+      ]})
   ]
 };
